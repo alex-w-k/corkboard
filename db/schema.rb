2b4ccd5f5f21f332315092d7f677f2ce86663088
Ilana Corson <ilana@turing.io>
2017-07-20 11:28:23 -0600

    add corkboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525014026) do
+ActiveRecord::Schema.define(version: 20170525055725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20170525014026) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "service_ids",              array: true
-    t.string   "radius"
     t.index ["user_id"], name: "index_pro_services_on_user_id", using: :btree
   end
 
@@ -121,8 +120,8 @@ ActiveRecord::Schema.define(version: 20170525014026) do
     t.string   "email"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.string   "type"
     t.string   "uid"
+    t.string   "type"
     t.string   "password"
     t.string   "password_digest"
     t.string   "verification_code"
