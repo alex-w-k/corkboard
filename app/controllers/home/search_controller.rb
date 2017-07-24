b56db520e18998b2719c8533bfaa8087f7f0471e
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-24 16:24:29 -0600

    adds functionality to dynamically search for industry, category or service

--- app/controllers/home/search_controller.rb
+++ app/controllers/home/search_controller.rb
@@ -1,7 +1,17 @@
 class Home::SearchController < ApplicationController
   def index
-    category = Category.where('name ILIKE ?', "#{params[:service_search]}").first
-    binding.pry
-    redirect_to hire_industry_category_path(category.industry.slug, category.slug)
+    industry = Industry.where('name ILIKE ?', "#{search_params}").first
+    category = Category.where('name ILIKE ?', "#{search_params}").first
+    service = Service.where('name ILIKE ?', "#{search_params}").first
+    
+    return redirect_to new_hire_project_path(service) if service
+    return redirect_to hire_industry_category_path(category.industry.slug, category.slug) if category
+    return redirect_to hire_industry_path(industry) if industry
+    
+  end
+
+  private
+  def search_params
+    params[:service_search]
   end
 end
--- spec/features/guest/guest_can_find_services_with_search_bar_spec.rb
+++ spec/features/guest/guest_can_find_services_with_search_bar_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "As a guest" do
+  it "can browse industry with the search bar" do
+    industry = create(:industry, name: 'Events')
+
+    visit root_path
+    fill_in :service_search, with: "Events"
+    click_on "Get Started"
+    
+    expect(current_path).to eq(hire_industry_path(industry))
+  end
+
   it "can browse services with the search bar" do
     industry = create(:industry, name: 'Events')
     category = create(:category, name: 'Driver', industry_id: industry.id)
@@ -11,4 +21,16 @@ RSpec.describe "As a guest" do
     
     expect(current_path).to eq(hire_industry_category_path(industry, category))
   end
+
+  it "can browse services with search bar" do
+    industry = create(:industry, name: 'Events')
+    category = create(:category, name: 'Driver', industry_id: industry.id)
+    service = create(:service, name:"Personal Driver", category_id: category.id)
+    visit root_path
+    fill_in :service_search, with: "Personal Driver"
+    click_on "Get Started"
+
+    expect(current_path).to eq(new_hire_project_path(service))
+  end
+
 end
