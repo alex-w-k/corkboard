7cd1a0cb381b04af8cc0445e24346402fbc40376
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-24 17:06:57 -0600

    routes nav search through home search controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,16 +12,5 @@ module ApplicationHelper
       link_to 'Sign Up', choose_account_path
     end
   end
-
-  def navbar_service_search
-    if logged_in?
-      "<input type='text' class='form-control' placeholder='What service do you need today, #{current_user.first_name}?'>".html_safe
-    end
-  end
-
-  def navbar_search_button
-    if logged_in?
-      "<button type='submit' class='btn btn-default'><i class='glyphicon glyphicon-search'></i></button>".html_safe
-    end
-  end
+  
 end
