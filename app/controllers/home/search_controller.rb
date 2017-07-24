cfd2116315f4a0d48f8117b623029e98d9c343f7
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-24 16:41:05 -0600

    fixes margins

--- app/controllers/home/search_controller.rb
+++ app/controllers/home/search_controller.rb
@@ -1,8 +1,8 @@
 class Home::SearchController < ApplicationController
   def index
-    industry = Industry.where('name ILIKE ?', "#{search_params}").first
-    category = Category.where('name ILIKE ?', "#{search_params}").first
-    service = Service.where('name ILIKE ?', "#{search_params}").first
+    industry  =   Industry.where('name ILIKE ?', "#{search_params}").first
+    category  =   Category.where('name ILIKE ?', "#{search_params}").first
+    service   =   Service.where('name ILIKE ?', "#{search_params}").first
     
     return redirect_to new_hire_project_path(service) if service
     return redirect_to hire_industry_category_path(category.industry.slug, category.slug) if category
@@ -11,7 +11,8 @@ class Home::SearchController < ApplicationController
   end
 
   private
-  def search_params
-    params[:service_search]
-  end
+    
+    def search_params
+      params[:service_search]
+    end
 end
