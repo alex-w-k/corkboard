1e0014cbec35ea3a103bad5d85294583613e4bcc
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-25 10:20:45 -0600

    adds model test for attachment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,12 +1,12 @@
 class Attachment < ApplicationRecord
   belongs_to :attachable, polymorphic: true
-  has_attached_file :upload
 
+  has_attached_file :upload
   has_attached_file :upload, :styles => {
                                         :thumb => "100x100#",
                                         :medium => "200x200",
                                         }
-
+  validates :upload, presence: true
   validates_attachment_content_type :upload, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   
 end
