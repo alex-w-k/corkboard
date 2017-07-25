85bd25d00416e7d924a1618d556613a3e7486482
Alex Koch <alex549us3@gmail.com>
2017-07-24 21:48:32 -0600

    add authy gem and start setting up 2fa the right way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.uid = session[:omniauth_info]['uid'] if omniauth_user
     if @user.save
-      ConfirmationSender.send_confirmation_to(@user)
       session[:user_id] = @user.id
+      authy = Authy::API.register_user(
+        email: @user.email,
+        cellphone: @user.phone_number,
+        country_code: @user.country_code
+      )
+      @user.update(authy_id: authy.id)
+      Authy::API.request_sms(id: @user.authy_id)
       session.delete(:omniauth_info)
       redirect_to twilio_confirmation_path
     else
@@ -20,15 +26,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def verify
+    @user = current_user
+    token = Authy::API.verify(id: @user.authy_id, token: params[:token])
+    if token.ok?
+      @user.update(verified: true)
+      flash[:success] = "You successfully verified your account!"
+      send_message('You successfully verified your account!')
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:danger] = "Incorrect code, please try again"
+      render :show_verify
+    end
+  end
+
+  def resend
+    @user = current_user
+    Authy::API.request_sms(id: @user.authy_id)
+    flash[:notice] = 'Verification code re-sent'
+    redirect_to verify_path
+  end
+
+
   private
 
   def user_params
     params.require(:user).permit(:first_name,
                                  :last_name,
                                  :zipcode,
+                                 :country_code,
                                  :phone_number,
                                  :email,
                                  :password,
                                  :password_confirmation)
   end
+
+  def send_message(message)
+    @user = current_user
+    twilio_number = ENV['TWILIO_NUMBER']
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_AUTH_TOKEN']
+    message = @client.api.accounts(account_sid).messages.create(
+      :from => twilio_number,
+      :to => @user.country_code+@user.phone_number,
+      :body => message
+    )
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721145145) do
+ActiveRecord::Schema.define(version: 20170724215539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,6 +126,8 @@ ActiveRecord::Schema.define(version: 20170721145145) do
     t.string   "password"
     t.string   "password_digest"
     t.string   "verification_code"
+    t.integer  "country_code"
+    t.integer  "authy_id"
   end
 
   add_foreign_key "bids", "projects"
