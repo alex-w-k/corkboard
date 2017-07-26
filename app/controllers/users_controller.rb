06e27090f72da20e0d0458dc84f7af6211b98ee4
Alex Koch <alex549us3@gmail.com>
2017-07-26 13:47:36 -0600

    back to capistrano basics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @oauth_info = OauthParse.new(session[:omniauth_info])
-    @country_codes = ISO3166::Country.all.map do |countries|
-      counries.country_code
-    end
-    binding.pry
   end
 
   def create
