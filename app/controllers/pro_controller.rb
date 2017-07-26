de8ffeff0f669058c1db1369476a467ef6fb9b99
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-26 14:17:38 -0600

    fixes pro login

--- app/controllers/pro_controller.rb
+++ app/controllers/pro_controller.rb
@@ -4,6 +4,7 @@ class ProController < ApplicationController
   end
 
   def new
+    binding.pry
     @oauth_info = OauthParse.new(session[:omniauth_info])
     @services = Service.where(id: params[:service_id])
     @radius = params[:radius].empty? ? "10" : params[:radius]
@@ -15,18 +16,30 @@ class ProController < ApplicationController
   def create
     @pro = Pro.new(pro_params)
     @pro.uid = session[:omniauth_info]['uid'] if omniauth_user
+
     if @pro.save
       session[:service_ids].each do |id|
         @pro.pro_services.create(service_id: id, radius: session[:radius])
       end
-      ConfirmationSender.send_confirmation_to(@pro)
-      session[:user_id] = @pro.id
+       session[:user_id] = @pro.id
+      authy = Authy::API.register_user(
+        email: @pro.email,
+        cellphone: @pro.phone_number,
+        country_code: @pro.country_code
+      )
+      @pro.update(authy_id: authy.id)
+      Authy::API.request_sms(id: @pro.authy_id)
       session.delete(:service_ids)
       session.delete(:radius)
       session.delete(:omniauth_info)
+      binding.pry
       redirect_to verify_path
     else
       flash.now[:danger] = @pro.errors.full_messages
+      binding.pry
+      @services = Service.where(id: session[:service_ids])
+      @radius = session[:radius]
+      # @pro = Pro.new
       render :new
     end
   end
