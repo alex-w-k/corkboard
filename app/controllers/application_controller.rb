ec9b0ad58539514aff9275edf3860326e57b881d
Alex Koch <alex549us3@gmail.com>
2017-07-27 14:15:33 -0600

    controller typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery, with: :exception, prepend: true
+  protect_from_forgery with: :exception, prepend: true
 
   helper_method :current_user
   helper_method :logged_in?
