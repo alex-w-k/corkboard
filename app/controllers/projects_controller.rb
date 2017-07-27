5afef018b77e9ad3b0acec1a20ca84a8b6d2bacb
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-27 17:11:16 -0600

    Create review model and routes, troubleshoot form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def show
     @project = current_user.projects.where(id: params[:id]).first
+    @review = @project.build_review
     render file: 'public/404.html' unless @project
 
   end
