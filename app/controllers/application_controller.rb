0af3f9a270cfab2724cc9fe8ecebcc7dd420620f
Alex Koch <alex549us3@gmail.com>
2017-07-27 14:11:30 -0600

    application controller fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery, with: :exception, prepend: true
+
   helper_method :current_user
   helper_method :logged_in?
   helper_method :omniauth_user
   helper_method :user_redirect
-  protect_from_forgery prepend: true
 
   include FlashMessages
   
