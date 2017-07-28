212b326a5926cf34a9a40ea665779470d26d94a2
Padraic O'Neill <pconeill@gmail.com [core]>
2017-07-28 10:22:56 -0600

    temporarily removes bid withdrawal test

--- spec/features/pro/pro_user_can_jobs_and_bids_and_statuses_spec.rb
+++ spec/features/pro/pro_user_can_jobs_and_bids_and_statuses_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'A pro user can withdraw a bid' do
-
-  it 'has an open bid and can withdraw' do
-    pro = create(:pro_user)
-    user = create(:user)
-    project1 = create(:project)
-    project2 = create(:project, status: 1)
-    project3 = create(:project, status: 2)
-
-    visit '/pro-dashboard'
-
-    click_link 'Open'
-    #expect(page).to have_content("Owner: #{}")
-    # Then I see all my projects organized by open, bid on, lost, in progress or completed.
-    # And when I click on a job
-    # I am on the job's show page
-    # And I can mark the job complete, cancelled, or offer a message/update to the user.
-    #   end
-    # end
-
-  end
-end
+# RSpec.describe 'A pro user can withdraw a bid' do
+#
+#   it 'has an open bid and can withdraw' do
+#     pro = create(:pro_user)
+#     user = create(:user)
+#     project1 = create(:project)
+#     project2 = create(:project, status: 1)
+#     project3 = create(:project, status: 2)
+#
+#     visit '/pro-dashboard'
+#
+#     click_link 'Open'
+#     #expect(page).to have_content("Owner: #{}")
+#     # Then I see all my projects organized by open, bid on, lost, in progress or completed.
+#     # And when I click on a job
+#     # I am on the job's show page
+#     # And I can mark the job complete, cancelled, or offer a message/update to the user.
+#     #   end
+#     # end
+#
+#   end
+# end
