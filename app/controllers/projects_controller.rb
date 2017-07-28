4ea0fd1e7090c316b3a60195dbaa872e5fa366d3
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-27 18:04:43 -0600

    Move review scss to _reviews.scss

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,9 @@
 class ProjectsController < ApplicationController
   def show
     @project = current_user.projects.where(id: params[:id]).first
-    if @project
+    render file: 'public/404.html' if @project.nil?
+    if @project && @project.review.nil?
       @review = @project.build_review
-    else
-      render file: 'public/404.html'
     end
   end
 end
