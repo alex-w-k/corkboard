1e05e266c976f0c7512dc24a6fa8d5ebed59d07f
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-28 08:17:56 -0600

    Move star rating logic from review partial to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  def star_rated
+    ("<i class='fa fa-star' aria-hidden='true'></i>" * (self.rating)).html_safe
+  end
+
+  def star_denied
+    ("<i class='fa fa-star' aria-hidden='true'></i>" * (5 - self.rating)).html_safe
+  end
 end
