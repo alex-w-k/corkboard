06fbbf08f76892e65bcf946bb8e52c023fc9f808
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-29 09:02:09 -0600

    create html variables in review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,13 @@ class Review < ApplicationRecord
   belongs_to :project
 
   def star_rated
-    ("<i class='fa fa-star star-rated' aria-hidden='true'></i>" * (self.rating.round)).html_safe
+    yellow_star_html = "<i class='fa fa-star star-rated' aria-hidden='true'></i>"
+    (yellow_star_html * self.rating.round).html_safe
   end
 
   def star_denied
-    ("<i class='fa fa-star star-denied' aria-hidden='true'></i>" * (5 - self.rating.round)).html_safe
+    gray_star_html = "<i class='fa fa-star star-denied' aria-hidden='true'></i>"
+    (gray_star_html * (5 - self.rating.round)).html_safe
   end
+
 end
