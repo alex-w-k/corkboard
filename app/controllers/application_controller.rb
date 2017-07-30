89cbdec4013c102ae8db64f1062c7fb4a5d9a0a0
Alex Koch <alex549us3@gmail.com>
2017-07-29 20:56:27 -0600

    changing force_ssl in production.rb to true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  force_ssl if: "Rails.env.production? && action_name != 'messages'"
 
   helper_method :current_user
   helper_method :logged_in?
