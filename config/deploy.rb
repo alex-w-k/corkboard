b0ac6b624c55ae52165cf9be46ab18e5b9b9f781
Alex Koch <alex549us3@gmail.com>
2017-07-29 20:19:11 -0600

    changing actioncable address

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "corkboard"
 set :repo_url, "git@github.com:Benjaminpjacobs/corkboard.git"
 set :use_sudo, true
 
-set :branch, 'capistrano'
+set :branch, 'rails-upgrade-capistrano'
 
 # Default branch is :setup-capistrano
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
