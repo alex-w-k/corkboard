a9bcb00f2a56e5eb00ef7cdb0515be62ba7a037a
Alex Koch <alex549us3@gmail.com>
2017-07-29 20:47:21 -0600

    actioncable ssl redirect fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
+  force_ssl if: :ssl_configured?
 
   helper_method :current_user
   helper_method :logged_in?
@@ -30,6 +31,12 @@ class ApplicationController < ActionController::Base
     else
       redirect_to profile_dashboard_path
     end
+  end
+
+  private
+
+  def ssl_configured?
+    Rails.env.in?(Rails.application.config.secure_envs) && request.path != '/cable'
   end
 
 end
