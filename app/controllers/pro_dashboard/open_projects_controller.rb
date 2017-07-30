c0a31e25ed421e01e044c367db4778cf7983cf4f
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-30 08:55:14 -0600

    adds autofill to navsearch

--- app/controllers/pro_dashboard/open_projects_controller.rb
+++ app/controllers/pro_dashboard/open_projects_controller.rb
@@ -2,13 +2,13 @@ class ProDashboard::OpenProjectsController < ApplicationController
   attr_accessor :to_bid_on
 
   def index
-    @pro = Pro.find(current_user.id)
-    @pro_location = Geokit::Geocoders::GoogleGeocoder.geocode(@pro.zipcode)
-    @open_projects = @pro.open_projects
+    pro = Pro.find(current_user.id)
+    # binding.pry
+    pro_location = Geokit::Geocoders::GoogleGeocoder.geocode(pro.zipcode)
+    # @open_projects = @pro.open_projects
 
-    @to_bid_on = []
-
-    @open_projects.each do |open_project|
+    @to_bid_on = Project.open.map do |open_project|
+      binding.pry
       request_location = open_project.zipcode
       distance = @pro_location.distance_to(request_location)
         @to_bid_on << open_project
