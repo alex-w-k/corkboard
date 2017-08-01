9b7bb0c71cf498e094efccab1c851b1f03bfdc0d
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-08-01 17:17:39 -0600

    Change view details link to be a button, style. Change Javascript to load on turbolinks load. Page was not loading javascript the first time and loading on turbolinks fixed the bug.

--- app/assets/javascripts/user_dashboard.js
+++ app/assets/javascripts/user_dashboard.js
@@ -1,6 +1,6 @@
 var API = 'http://localhost:3000/api/v1/projects';
 
-$(document).ready(function(){
+document.addEventListener("turbolinks:load", function() {
   getOpenProjects();
   getAcceptedProjects();
   getCompletedProjects();
@@ -15,14 +15,18 @@ var submitClose = function() {
 }
 
 var getOpenProjects = function() {
-  $('.open').empty();
+  $('.open-project').empty();
   return $.ajax({
     url: API + `/find_all?requester_id=${user}&status=0`,
     method: 'GET',
   }).done(function(projects) {
-    projects.forEach(function(project){
-      $('.open').append('<li class="list-group-item"><p><a href="/projects/' + project.id + '">View Project and Bids</a></p><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p></li>')
-    })
+    if (projects.length > 0) {
+      projects.forEach(function(project){
+        $('.open-project').append('<li class="list-group-item"><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p><p><a href="/projects/' + project.id + '" class="btn btn-primary" role="button">View Details and Bids</a></p></li>')
+      })
+    } else {
+      $('.open-project').append('<li class="list-group-item"><p>You have no open projects</p>')
+    }
   }).fail(function(error) {
     console.log(error);
   })
@@ -36,13 +40,17 @@ var getAcceptedProjects = function() {
     url: API + `/find_all?requester_id=${user}&status=1`,
     method: 'GET',
   }).done(function(projects) {
-    projects.forEach(function(project){
-      $('.accepted').append('<li class="list-group-item"><p><a href="/projects/' + project.id + '">View Project and Bids</a><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p><p><form class="close-form"><input type="hidden" name="token" value=' + token + '><input type="hidden" name="project_id" value="' + project.id + '"><input type="submit" value="Mark as Complete" class="btn btn-warning"></form></p></li>')
-    })
-    submitClose();
-  }).fail(function(error) {
-    console.log(error);
-  })
+    if (projects.length > 0) {
+      projects.forEach(function(project){
+        $('.accepted').append('<li class="list-group-item"><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p><p><form class="close-form"><input type="hidden" name="token" value=' + token + '><input type="hidden" name="project_id" value="' + project.id + '"><span class="accepted-buttons"><input type="submit" value="Mark as Complete" class="btn btn-warning"></form><a href="/projects/' + project.id + '" class="btn btn-primary" role="button">View Details and Bids</a></span></p></li>')
+      })
+      submitClose();
+    } else { 
+      $('.accepted').append('<li class="list-group-item"><p>You have no accepted projects</p>')
+    }
+      }).fail(function(error) {
+        console.log(error);
+      })
 };
 
 var getCompletedProjects = function() {
@@ -51,9 +59,13 @@ var getCompletedProjects = function() {
     url: API + `/find_all?requester_id=${user}&status=2`,
     method: 'GET',
   }).done(function(projects) {
-    projects.forEach(function(project){
-      $('.closed').append('<li class="list-group-item"><p><a href="/projects/' + project.id + '">View Project and Bids</a></p><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p></li>')
-    })
+    if (projects.length > 0) {
+      projects.forEach(function(project){
+        $('.closed').append('<li class="list-group-item"><p>Service: ' + project.service.name + '</p><p>Description: ' + project.description + '</p><p>Timeline: ' + project.timeline + '</p><p><a href="/projects/' + project.id + '" class="btn btn-primary" role="button">View Details and Bids</a></p></li>')
+      })
+    } else { 
+      $('.closed').append('<li class="list-group-item"><p>You have no closed projects</p>')
+    }
   }).fail(function(error) {
     console.log(error);
   })
