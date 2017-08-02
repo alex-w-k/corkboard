8c8fbc45b5efdd49512f6970aac9437f6d66ff39
Alex Koch <alex549us3@gmail.com>
2017-08-02 08:43:28 -0600

    fixing ssl ajax bug

--- app/assets/javascripts/components/search_form.jsx
+++ app/assets/javascripts/components/search_form.jsx
@@ -3,7 +3,7 @@ var SearchForm = React.createClass({
     var query = document.getElementById('query').value
     var self = this;
     $.ajax({
-      url: 'https://api.myfucking.network/api/v1/records',
+      url: 'https://api.myfucking.network/api/v1/records/',
       data: { query: query },
       success: function(data) {
         self.props.handleSearch(data);
