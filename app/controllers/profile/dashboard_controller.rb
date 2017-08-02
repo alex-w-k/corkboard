b7359c8c4a6c29172cf4798392f5baefe3023bb7
Alex Koch <alex549us3@gmail.com>
2017-08-02 11:32:33 -0600

    removed pry

--- app/controllers/profile/dashboard_controller.rb
+++ app/controllers/profile/dashboard_controller.rb
@@ -2,6 +2,5 @@ class Profile::DashboardController < ApplicationController
   def show
     @projects = current_user.projects
     @token = JsonWebToken.encode({id: current_user.id})
-    binding.pry
   end
 end
