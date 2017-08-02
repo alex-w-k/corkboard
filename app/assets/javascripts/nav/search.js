ea9faf8d0aa50380cf3ec6ee7b62f55a5b4a504b
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-01 19:37:07 -0600

    adds conditional to search bar

--- app/assets/javascripts/nav/search.js
+++ app/assets/javascripts/nav/search.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-  const searchInputField = document.getElementById('service_search')
-  const dataList = document.getElementById('services_list')
+    const searchInputField = document.getElementById('service_search')
+    const dataList = document.getElementById('services_list')
+    if (searchInputField) {
 
-  searchInputField.addEventListener("keyup", function() {
-    search = this.value
-    $.getJSON('api/search', { query: search },
-      function(data) {
-        if (data) {
-          dataList.innerHTML = ''
-          data.forEach(function(datum) {
-            var option = document.createElement('option')
-            option.value = datum.name
-            dataList.appendChild(option)
-          });
-        }
-      })
-  })
+        searchInputField.addEventListener("keyup", function() {
+            search = this.value
+            $.getJSON('api/search', { query: search },
+                function(data) {
+                    if (data) {
+                        dataList.innerHTML = ''
+                        data.forEach(function(datum) {
+                            var option = document.createElement('option')
+                            option.value = datum.name
+                            dataList.appendChild(option)
+                        });
+                    }
+                })
+        })
+    }
 })
