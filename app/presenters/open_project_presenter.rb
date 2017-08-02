e6b4308c97de463090b850f377188eaae1391810
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-01 20:19:17 -0600

    only bids for which pro hasn't bid show up on find projects

--- app/presenters/open_project_presenter.rb
+++ app/presenters/open_project_presenter.rb
@@ -3,7 +3,7 @@ class OpenProjectPresenter < SimpleDelegator
   def to_bid_on
     @to_bid_on ||= self.open_projects.select do |open_project|
       distance = self.distance_to(open_project)
-      distance < self.radius
+      (distance < self.radius) && self.in?(open_project.pros)
     end
   end
   
