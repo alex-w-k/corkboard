f61bb9b74dcda0357004f4e145a19a2ff8b94b88
Padraic O'Neill <pconeill@gmail.com [core]>
2017-08-03 15:11:14 -0600

    adjust seed file, adds pro#accepted_bid_project

--- app/models/pro.rb
+++ app/models/pro.rb
@@ -6,13 +6,17 @@ class Pro < User
   geocoded_by :zipcode
   after_validation :geocode          # auto-fetch coordinates
 
-  
+
   # def services
   #   Service.where(id: pro_service[:service_ids])
   # end
 
   def open_projects
     Project.where(status: :open, service_id: services)
+  end
+
+  def accepted_bid_projects
+    Project.where(status: :accepted, service_id: services)
   end
 
   def radius
