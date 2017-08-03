c25c654bf45c9f0207172c284902addac166562a
Padraic O'Neill <pconeill@gmail.com [core]>
2017-08-02 18:21:03 -0600

    fix typo in appearance.js

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -1,4 +1,4 @@
-App.appearance = App.cable.subscriptions.create("AppearanceChannel", {
+App.cable.subscriptions.create("AppearanceChannel", {
   connected: function() {},
   disconnected: function() {},
   received: function(data) {
